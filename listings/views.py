41f5e3400d623e255008983c82ed0a9adbf47eb2
thatcarguy <5158433+thatcarguy@users.noreply.github.com>
2021-01-03 16:35:52 -0700

    pagination and realtors about

--- listings/views.py
+++ listings/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
+
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
     context = {
-        'listings':listings
+        'listings':paged_listings
     }
     return render(request,'listings/listings.html',context)
 
-def listing(request):
-    return render(request,'listings/listing.html')
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    # context is the response body returned to template
+    context = {
+        'listing':listing
+    }
+    return render(request,'listings/listing.html',context)
 
 def search(request):
     return render(request,'listings/search.html')
